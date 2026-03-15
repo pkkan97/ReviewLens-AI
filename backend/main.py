2eb2c39018417cb9ddde1ca2fc25a37a0f563665
palakh.kanwar <palakh.kanwar@fractal.ai>
2026-03-15 15:55:40 +0530

    updated the port for health check of railway app

--- backend/main.py
+++ backend/main.py
@@ -399,4 +399,5 @@ async def create_session():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
